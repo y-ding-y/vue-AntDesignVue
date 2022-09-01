68a3d0597abdaf3ed8239a9134ea9ed7715c6344
Sarah Hey <49681619+y-ding-y@users.noreply.github.com>
2022-09-01 08:50:31 +0800

    Create element.js

---
+++ utils/element.js
@@ -0,0 +1,36 @@
+// element.js 全文内容如下，按自己需要引入就好了
+import Vue from 'vue'
+import {
+    ColorPicker,
+    Collapse,
+    CollapseItem,
+    Slider,
+    Radio,
+    RadioGroup,
+    RadioButton,
+    Button,
+    ButtonGroup,
+    Select,
+    Option,
+    Divider,
+    Table,
+    TableColumn
+} from 'element-ui'
+
+import '@/assets/css/theme-green/index.css' // 浅绿色主题
+import '@/assets/css/icon.css'
+
+Vue.use(ColorPicker)
+Vue.use(Collapse)
+Vue.use(CollapseItem) 
+Vue.use(Slider) 
+Vue.use(Radio) 
+Vue.use(RadioGroup) 
+Vue.use(RadioButton) 
+Vue.use(Button) 
+Vue.use(ButtonGroup) 
+Vue.use(Select) 
+Vue.use(Option) 
+Vue.use(Divider) 
+Vue.use(Table) 
+Vue.use(TableColumn) 
